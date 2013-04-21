101e7d47c101d60d935d863e13a34fe2c776fba3
Washington Luiz <huoxito@gmail.com>
2013-04-21 03:26:37 -0300

    Allow users to not override default mailer configs
    Fix from_address logic in mailers, it broke when there were no
    mail_method record on the database.
    
    Cherry-picked from spree/1-3-stable.
    
    Change-Id: Ieb090c923ba248832d946337f9f912571fc38847

--- core/app/models/spree/app_configuration.rb
+++ core/app/models/spree/app_configuration.rb
@@ -56,7 +56,6 @@ module Spree
     preference :layout, :string, :default => 'spree/layouts/spree_application'
     preference :logo, :string, :default => 'admin/bg/spree_50.png'
     preference :max_level_in_taxons_menu, :integer, :default => 1 # maximum nesting level in taxons menu
-    preference :max_quantity, :integer, :default => 1000 # Maximum allowable quantity when checking out
     preference :override_actionmailer_config, :boolean, :default => true
     preference :orders_per_page, :integer, :default => 15
     preference :prices_inc_tax, :boolean, :default => false
